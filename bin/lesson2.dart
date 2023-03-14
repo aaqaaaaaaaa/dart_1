0e72e19be30e880d5abfff85881931438fa35d7a
Jalolxon <85275641+aaqaaaaaaaa@users.noreply.github.com>
2023-03-14 10:18:36 +0500

    Initial commit

---
+++ bin/lesson2.dart
@@ -0,0 +1,99 @@
+void main(List<String> args) {
+// ==, <, >, <=, >=, !=,
+// && , ||
+
+  // if (ball < 100) {
+  //   if (ball >= 80 && ball <= 100) {
+  //     print('A\'lo');
+  //   } else if (ball >= 60 && ball < 80) {
+  //     print('Yaxshi');
+  //   } else {
+  //     print('Qoniqarsiz');
+  //   }
+  // } else {
+  //   print('Xatolik');
+  // }
+  // if (ball == 5) {
+  //   print('A\'lo');
+  // } else if (ball == 4) {
+  //   print('yaxshi');
+  // } else if (ball == 3) {
+  //   print('qoniqarli');
+  // } else {
+  //   print('Bunday ball mavjud emas');
+  // }
+
+  // if (true) {
+  //   print('Salom');
+  // }
+
+  // int ball = 49;
+  // if (ball < 50 || ball > 59) {
+  //   print(true);
+  // }
+
+//   String parol = "qwerty";
+
+//   // if (parol.length >= 8) {
+//   //   print(true);
+//   // } else {
+//   //   print(false);
+//   // }
+//   parol = "qwerty";
+//   // print(parol.contains(''));
+//   // print(parol.compareTo('qwerty'));
+//   // print(parol.indexOf('y'));
+
+// // else if
+// // else
+// // &&  ||
+//   int ball = 100;
+
+//   if (ball < 100) {
+//     if (ball >= 80 && ball <= 100) {
+//       print('A\'lo');
+//     } else if (ball >= 60 && ball < 80) {
+//       print('Yaxshi');
+//     } else {}
+//   } else {
+//     print('Xatolik');
+//   }
+
+//   switch (ball) {
+//     case 80:
+//       print('A\'lo');
+//       break;
+//     case 4:
+//       print('Yaxshi');
+//       break;
+//     case 3:
+//       print('Qoniqarli');
+//       break;
+//     default:
+//       print('Qoniqarsiz');
+//   }
+
+// 0 - 14 , 15-29, 30-45, 46 -59
+  // 1        2     3      4
+
+  // int min = 25;
+  // if (min >= 0 && min < 15) {
+  //   print('1');
+  // } else if (min >= 15 && min <= 29) {
+  //   print('2');
+  // }
+
+  bool a = true;
+  // emas degani uning teskari qaymati. Masalan a = true
+  // bo'lsa uni teskari qiymati  false bo'ladi
+  print('a = $a');
+  print('a  ning teskarisi ${!a}');
+
+
+
+  int b = 125;
+  print('Javob: ${(b / 100).floor()}');
+
+
+  
+}
